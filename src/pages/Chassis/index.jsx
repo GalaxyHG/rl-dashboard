571a8a8395b8ce63d420c9b1900099d6834822d2
GalaxyHG <hrezende0207@gmail.com>
2024-06-29 13:58:54 -0300

    pages main file, card main file

---
+++ src/pages/Chassis/index.jsx
@@ -0,0 +1,9 @@
+function Chassis() {
+    return (
+        <>
+            <h1>This is the chassis page</h1>
+        </>
+    )
+}
+
+export default Chassis
---
+++ src/pages/Pass/index.jsx
@@ -0,0 +1,9 @@
+function Pass() {
+    return(
+        <>
+            <h1>This is the Pass page</h1>
+        </>
+    )
+}
+
+export default Pass
