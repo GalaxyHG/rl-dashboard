539a49434e4a8a38b4c5d072131e5dd44b77eb2e
Helena Rezende <99421906+GalaxyHG@users.noreply.github.com>
2024-07-04 12:49:09 +0000

    a api funfou gg

--- src/pages/Chassis/index.jsx
+++ src/pages/Chassis/index.jsx
@@ -6,15 +6,24 @@ function Chassis() {
   const [chassis, setChassis] = useState([]);
 
   useEffect(() => {
-    const buscarChassis = async () => {
-      const response = await fetch(
-        "https://raw.githubusercontent.com/GalaxyHG/api-rocketleague/main/chassis.json"
-      );
-      const data = await response.json();
-      setChassis(data);
-    };
-    buscarChassis();
+    fetch('https://raw.githubusercontent.com/GalaxyHG/api-rocketleague/main/chassis.json')
+    .then(response => response.json())
+    .then(data => {
+      setChassis(data.chassis);
+    })
+    .catch(error => console.error('Error:', error));
   }, []);
+
+  if (chassis == null) {
+    return (
+      <>
+        <section className={styles.mainSection}>
+          <h1>This is the Chassis page</h1>
+          <p>Carregando chassis...</p>
+        </section>
+      </>
+    )
+  }
 
   return (
     <>
@@ -24,7 +33,7 @@ function Chassis() {
           <section className={styles.cardsContainer}>
             {chassis.map((chassi) => (
               <Card
-                key={chassi.id}
+                // key={chassi.id}
                 carName={chassi.name}
                 rarity={chassi.rarity}
                 imgCar={chassi.image}
