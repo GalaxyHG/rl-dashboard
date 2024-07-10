6190952eb89899607d0b50d49f1d152b0150d2d5
Helena Rezende <99421906+GalaxyHG@users.noreply.github.com>
2024-07-10 11:26:34 +0000

    page 404 ready

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -1,13 +1,22 @@
 import styles from './Page404.module.css'
+import octane404 from '../../assets/images/404octane.png'
+import { Link } from 'react-router-dom'
+import arrow from '../../assets/icons/arrow-forward.svg'
 
 function Page404() {
     return (
         <>
-            <h2 className={styles.titulo2}>Algo de errado não está certo!</h2>
-            <div className={styles.textos}>
-                <span className={styles.texto_grande}>404</span> <br />
-                <strong className={styles.texto_vermelho}>Página Não Localizada!</strong>
-            </div>
+            <section className={styles.container}>
+                <div className={styles.container404}>
+                    <div>
+                        <h1 className={styles.t404}>404</h1>
+                        <h2 className={styles.h2404}>We didn't find this page...</h2>
+                        <h4 className={styles.h4404}>But don't worry, this is Rocket League!</h4>
+                    </div>
+                    <img src={octane404} />
+                </div>
+                <Link className={styles.goback} to='/'>Go back <img src={arrow} /></Link>
+            </section>
         </>
     )
 }
